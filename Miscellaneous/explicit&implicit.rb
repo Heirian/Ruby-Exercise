9ef4190b41a46318ccd3b5f5b65a02282705b746
Bruno Teófilo <shinkcc@gmail.com>
2017-05-24 22:44:33 -0300

    Update explicit&implicit.rb

--- Miscellaneous/explicit&implicit.rb
+++ Miscellaneous/explicit&implicit.rb
@@ -3,3 +3,9 @@
 def filter(array, block)
  return array.select(&block)
 end
+
+#Converting the implicit block into an explicit block
+
+Filter = lambda do |array, &block|
+ array.select(&block)
+end
